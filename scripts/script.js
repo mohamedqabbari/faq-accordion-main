b81b7123370dd3599ad6028e3954555ccbb13e91
Mohammed Qabbari <mqabbari77@gmail.com>
2024-03-26 12:49:27 +0200

    First Update of Frontend Mentor Challenge: Some Modification in JavaScript File

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,18 @@
 let question = [...document.querySelectorAll(".question")];
 let answer = [...document.querySelectorAll(".answer")];
 let show_btn = [...document.querySelectorAll(".show")];
-let isFQAopened = true; //Checking if FAQ is opened
+let isFAQopened = true; //Checking if FAQ is opened
 for (let i = 0; i < question.length; i++) {
     show_btn[i].addEventListener("click", () => {
-        $(answer[i]).slideToggle('fast');
-        if (isFQAopened) {
+        for (let j = 0; j < question.length; j++) {
+            $(answer[j]).slideUp('fast');
+            show_btn[j].firstChild.src = "../assets/images/icon-plus.svg";
+            isFAQopened = true;
+        }
+        if (isFAQopened) {
             show_btn[i].firstChild.src = "../assets/images/icon-minus.svg";
-            isFQAopened = false; //If FQA was opened, image was changed and value of isFAQopened is changed to false
-        } else {
-            show_btn[i].firstChild.src = "../assets/images/icon-plus.svg";
-            isFQAopened = true; //If FAQ was already opened, isFAQopened has a false value, so skip true statement and change image to initial state
+            isFAQopened = false;
+            $(answer[i]).slideDown('fast');
         }
     });
-    
 }
